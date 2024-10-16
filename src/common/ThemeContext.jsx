cd129307992cbf266496e54ebd30f57296371201
arg387 <84933329+arg387@users.noreply.github.com>
2024-10-16 10:52:19 +0530

    nothing

--- src/common/ThemeContext.jsx
+++ src/common/ThemeContext.jsx
@@ -5,9 +5,7 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({children}) => {
-
   const [theme, setTheme] = useState('dark');
-
   useEffect(() => {
     document.body.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
